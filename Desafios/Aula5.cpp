da0466c03115709f7378f55b76ae919c8b388148
GuilhermeLondero <londero.guilherme@ufn.edu.br>
2025-08-25 23:38:20 +0000

    Aula 5

---
+++ Desafios/Aula5.cpp
@@ -0,0 +1,10 @@
+/* # Exercícios de Métodos
+
+1. **Contagem de letras em palavra**  
+   Fazer um programa e dentro dele um método que receba uma palavra (do tipo *string*) e uma letra (do tipo *char*).  
+   O método deve contar quantas vezes a letra aparece na palavra e exibir essa quantidade.  
+
+2. **Validação de datas**  
+   Fazer um programa e dentro dele um método que receba o dia (*string*), o mês (*string*) e o ano (*string*).  
+   O método deve escrever **"DATA VÁLIDA"** ou **"DATA INVÁLIDA"** para a situação das variáveis passadas.  
+*/
